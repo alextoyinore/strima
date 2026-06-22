c5e2b7ca9075c09379037a893e8ed84b319edae7
Alexander Ore <aore8030@gmai.com>
2026-06-22 09:29:16 +0100

    feat: add ffmpeg-static dependency to project

--- forge.config.ts
+++ forge.config.ts
@@ -9,7 +9,9 @@ import { FuseV1Options, FuseVersion } from '@electron/fuses';
 
 const config: ForgeConfig = {
   packagerConfig: {
-    asar: true,
+    asar: {
+      unpack: '**/node_modules/ffmpeg-static/**/*'
+    },
     executableName: 'strima',
     icon: './assets/icon'
   },
